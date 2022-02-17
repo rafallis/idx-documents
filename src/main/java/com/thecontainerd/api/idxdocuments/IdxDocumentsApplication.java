4784ae8d408c129b6dcfa308fc1f04d8926875ff
Rafael Jardim <rafael.jardim@camara.leg.br>
2022-02-17 20:00:27 -0300

    First commit

---
+++ src/main/java/com/thecontainerd/api/idxdocuments/IdxDocumentsApplication.java
@@ -0,0 +1,13 @@
+package com.thecontainerd.api.idxdocuments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdxDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdxDocumentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thecontainerd/api/idxdocuments/IdxDocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecontainerd.api.idxdocuments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdxDocumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
